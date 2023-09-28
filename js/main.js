97dfd3940f0f894f43f7a1884a5bd919c9ef576a
khalid Abdo <142664557+khalid-abdo@users.noreply.github.com>
2023-09-28 13:06:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let price =document.getElementById('price');
 let taxes =document.getElementById('taxes');
 let ads =document.getElementById('ads');
 let discount =document.getElementById('discount');
+
 let total =document.getElementById('total');
 let count =document.getElementById('count');
 let category =document.getElementById('category');
